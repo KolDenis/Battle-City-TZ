cb48494e064c52bc0b47be29ed845727b1482b53
DESKTOP-7RO48O1\kolot <kolotseidenis@gmail.com>
2023-06-20 13:32:57 +0300

    tank spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_Tanks;
+    [SerializeField]
+    private GameObject m_Floor;
+
+    private int m_NumberEnemys = 3;
+    private float m_FactorSizeTank = 0.3f;
+    void Start()
+    {
+        int labirintSize = LabirintManager.Instance.Sizelabirint;
+        float sizeCell = LabirintManager.Instance.SizeCell;
+        float offsetLabirint = LabirintManager.Instance.OffsetLabirint;
+
+
+        for (int i = 0; i < m_NumberEnemys; i++)
+        {
+            int cell = Random.Range(0, labirintSize* labirintSize);
+            GameObject prefabTank = m_Tanks[Random.Range(0, m_Tanks.Length)];
+            GameObject spawnedTank = Instantiate<GameObject>(prefabTank, m_Floor.transform);
+            spawnedTank.transform.localPosition = new Vector3(offsetLabirint + cell / labirintSize * sizeCell, offsetLabirint + cell % labirintSize * sizeCell, -1);
+            spawnedTank.transform.localScale = Vector3.one * sizeCell * m_FactorSizeTank;
+        }
+    }
+}
--- Assets/LabirintManager.cs
+++ Assets/LabirintManager.cs
@@ -9,31 +9,51 @@ public class LabirintManager : MonoBehaviour
     public static LabirintManager Instance;
 
     [SerializeField] 
-    private GameObject m_wall;
+    private GameObject m_Wall;
     [SerializeField]
-    private GameObject m_floor;
+    private GameObject m_Floor;
 
-    private Labirint labirint;
+    private Labirint m_Labirint;
 
-    private int sizelabirint = 15;
-    [SerializeField] private float sizeCell;
-    [SerializeField] private float offsetLabirint;
-    float weightWall = 0.01f;
+    private int m_Sizelabirint = 10;
+    private float m_SizeCell;
+    private float m_OffsetLabirint;
+    float m_WeightWall = 0.01f;
+
+    public int Sizelabirint
+    {
+        get
+        {
+            return m_Sizelabirint;
+        } 
+    }
+    public float SizeCell
+    {
+        get
+        {
+            return m_SizeCell;
+        }
+    }
+    public float OffsetLabirint
+    {
+        get
+        {
+            return m_OffsetLabirint;
+        }
+    }
 
     private void Awake()
     {
         Instance = this;
+
+        m_SizeCell = 1f / (float)m_Sizelabirint;
+        m_OffsetLabirint = -1f / 2f + m_SizeCell / 2;
     }
     // Start is called before the first frame update
     void Start()
     {
-        sizeCell = 1f / (float)sizelabirint;
-        offsetLabirint = -1f / 2f + sizeCell/2;
-
-        Debug.Log("labirint start");
-        labirint = new Labirint(sizelabirint);
-        labirint.generate_new_labirint();
-        Debug.Log("labirint");
+        m_Labirint = new Labirint(m_Sizelabirint);
+        m_Labirint.generate_new_labirint();
 
         ShowLabirint();
     }
@@ -41,29 +61,29 @@ public class LabirintManager : MonoBehaviour
     private void ShowLabirint()
     {
 
-        GameObject frame = Instantiate<GameObject>(m_wall, m_floor.transform);
+        GameObject frame = Instantiate<GameObject>(m_Wall, m_Floor.transform);
         frame.transform.localPosition = new Vector3(-0.5f, 0);
-        frame.transform.localScale = new Vector2(weightWall, 1);
+        frame.transform.localScale = new Vector2(m_WeightWall, 1);
 
-        frame = Instantiate<GameObject>(m_wall, m_floor.transform);
+        frame = Instantiate<GameObject>(m_Wall, m_Floor.transform);
         frame.transform.localPosition = new Vector3(0, -0.5f);
-        frame.transform.localScale = new Vector2(1, weightWall);
+        frame.transform.localScale = new Vector2(1, m_WeightWall);
 
-        for (int i = 0; i < labirint.lab.GetLength(0); i++)
+        for (int i = 0; i < m_Labirint.lab.GetLength(0); i++)
         {
-            for (int j = 0; j < labirint.lab.GetLength(1); j++)
+            for (int j = 0; j < m_Labirint.lab.GetLength(1); j++)
             {
-                if (labirint.lab[i, j, 0] == 1)
+                if (m_Labirint.lab[i, j, 0] == 1)
                 {
-                    GameObject wall = Instantiate<GameObject>(m_wall, m_floor.transform);
-                    wall.transform.localPosition = new Vector3(offsetLabirint + j * sizeCell + sizeCell / 2, offsetLabirint + i * sizeCell);
-                    wall.transform.localScale = new Vector2(weightWall, sizeCell);
+                    GameObject wall = Instantiate<GameObject>(m_Wall, m_Floor.transform);
+                    wall.transform.localPosition = new Vector3(m_OffsetLabirint + j * m_SizeCell + m_SizeCell / 2, m_OffsetLabirint + i * m_SizeCell);
+                    wall.transform.localScale = new Vector2(m_WeightWall, m_SizeCell);
                 }
-                if (labirint.lab[i, j, 1] == 1)
+                if (m_Labirint.lab[i, j, 1] == 1)
                 {
-                    GameObject wall = Instantiate<GameObject>(m_wall, m_floor.transform);
-                    wall.transform.localPosition = new Vector3(offsetLabirint + j * sizeCell, offsetLabirint + i * sizeCell + sizeCell / 2);
-                    wall.transform.localScale = new Vector2(sizeCell, weightWall);
+                    GameObject wall = Instantiate<GameObject>(m_Wall, m_Floor.transform);
+                    wall.transform.localPosition = new Vector3(m_OffsetLabirint + j * m_SizeCell, m_OffsetLabirint + i * m_SizeCell + m_SizeCell / 2);
+                    wall.transform.localScale = new Vector2(m_SizeCell, m_WeightWall);
                 }
                 Debug.Break();
             }
